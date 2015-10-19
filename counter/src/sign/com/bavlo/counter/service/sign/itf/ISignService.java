4fba7a02babedb0daa18222a83abb0556971d905
liuziyingcode <liuziyingcode@7cb93892-cc3c-4c95-95ab-c6227fb40d65>
2015-10-19 11:22:21 +0000

    签收单管理

---
+++ counter/src/sign/com/bavlo/counter/service/sign/itf/ISignService.java
@@ -0,0 +1,57 @@
+package com.bavlo.counter.service.sign.itf;
+
+import java.util.List;
+
+import com.bavlo.counter.model.sign.SignVO;
+
+/**
+ * @Title: 宝珑Counter
+ * @ClassName: ISignService 
+ * @Description: 签收单Service接口
+ * @author liuzy
+ * @date 2015-10-19 下午03:38:28
+ */
+public interface ISignService {
+	
+	/**
+	 * @Description: 签收单保存,返回主键
+	 * @param @return
+	 * @return Integer
+	 */
+	public Integer signSaveReID();
+	
+	/**
+	 * @Description: 签收单保存
+	 * @param @return
+	 * @return Integer
+	 */
+	public void signSave();
+	
+	/**
+	 * @Description: 更新
+	 * @param 
+	 * @return void
+	 */
+	public void signUpdate();
+	
+	/**
+	 * @Description: 通过ID查找数据，返回VO
+	 * @param @return
+	 * @return SignVO
+	 */
+	public SignVO findSingleById();
+	
+	/**
+	 * @Description: 通过条件查找
+	 * @param @return
+	 * @return List<SignVO>
+	 */
+	public List<SignVO> findListByWh();
+	
+	/**
+	 * @Description: 查找全部
+	 * @param @return
+	 * @return List<SignVO>
+	 */
+	public List<SignVO> findList();
+}
---
+++ counter/src/sign/com/bavlo/counter/web/sign/EntitySignController.java
@@ -0,0 +1,37 @@
+package com.bavlo.counter.web.sign;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bavlo.counter.model.sign.SignVO;
+import com.bavlo.counter.service.sign.itf.ISignService;
+import com.bavlo.counter.web.BaseController;
+
+/**
+ * @Title: 宝珑Counter
+ * @ClassName: EntitySignController 
+ * @Description: 实物签收单Controller
+ * @author liuzy
+ * @date 2015-10-19 下午03:30:17
+ */
+@Controller("entitySignController")
+@RequestMapping(value = "/entitysign")
+public class EntitySignController extends BaseController {
+
+	@Resource
+	private ISignService signService;
+
+	@RequestMapping(value="getAllSign")
+	public String getAllSign(){
+		System.out.println("正在查询签收单信息...");
+		
+		List<SignVO> listSign = signService.findList();
+		
+		
+		return "test";
+	}
+}
